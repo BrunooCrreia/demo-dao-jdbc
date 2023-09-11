38b7313831bd6b53712ad3c4ca4649adfa59b1c4
Bruno Correia <brun0.wilders@gmail.com>
2023-09-11 20:44:47 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,15 +11,29 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		//Department obj = new Department(1, "books");
-		//Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
-		
-		System.out.println("===Teste 1: seller findById ======");		
-		Seller seller = sellerdao.findById(3);
+		// Department obj = new Department(1, "books");
+		// Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00,
+		// obj);
 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println("===Teste 1: seller findById ======");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+
+		System.out.println("===Teste 2: seller findByDepartment ======");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+
+		}
+		System.out.println("===Teste 3: seller findAll ======");
+		 list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+
+		}
 
 	}
 }
